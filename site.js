af8c680854124fdc9a7a9106d9befed07ba59bf3
Sulay31 <47995373+Sulay31@users.noreply.github.com>
2019-03-26 20:50:13 -0400

    Add files via upload

---
+++ site.js
@@ -0,0 +1,21 @@
+console.log("External file");
+
+var city;
+var cityFrom = "";
+var cityName = "New York City";
+var x = 32;
+
+console.log ("cityName, x: ", cityName, x);
+
+var firstname = "sulayman", lastname = "larry";
+console.log("firstname, lastname", firstname, lastname);
+
+var s = "2019";
+var s2 = 2019;
+var n = 2001;
+console.log ("age: ", s - n);
+console.log ("age: ", s + n);
+console.log ('age:',  s2 + n );
+
+var escapequotes = "this is judi's" \"city\" name";
+console.log ("escapequotes:" , escapequotes);
